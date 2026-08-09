9617846f8bec4f14e10552b075bfbe47ecd53041
skkeletonn <theepicmailsoiongetconfused@gmail.com>
2026-08-09 15:58:06 +0200

    Create discord_invite.lua

---
+++ roblox/discord_invite.lua
@@ -0,0 +1,30 @@
+-- just works on pc ,, invites you to someone's discord
+local AllClipboards = setclipboard or toclipboard or set_clipboard or (Clipboard and Clipboard.set)
+local discordInvite = "https://discord.gg/WDbJ5wE2cR"
+
+local function openDiscordInvite()
+    local httpService = game:GetService("HttpService")
+    local request = syn and syn.request or http and http.request or http_request or fluxus and fluxus.request or getgenv().request or request
+    if request then
+        request({
+            Url = "http://127.0.0.1:6463/rpc?v=1",
+            Method = "POST",
+            Headers = {
+                ["Content-Type"] = "application/json",
+                ["Origin"] = "https://discord.com"
+            },
+            Body = httpService:JSONEncode({
+                cmd = "INVITE_BROWSER",
+                args = {
+                    code = discordInvite:gsub("https://discord.gg/", "")
+                },
+                nonce = httpService:GenerateGUID(false)
+            })
+        })
+    else
+        AllClipboards(discordInvite)
+        print("Discord invite copied to clipboard!")
+    end
+end
+
+openDiscordInvite()
